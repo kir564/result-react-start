36082f72fcf7a34ea0b39c5fc207900e9e303a91
kir564 <kir564ke@gmail.com>
2023-07-06 23:04:25 +0700

    app without jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -21,6 +22,19 @@ function App() {
       </header>
     </div>
   );
+}
+
+export function AppWithoutJSX() {
+  return (
+    React.createElement('div', {className: 'App'}, React.createElement(
+      'header', {className: 'App-header'}, [
+        React.createElement('img', {className: 'App-logo', alt: 'logo', src: logo, key: 1}),
+        React.createElement('p', {key: 2}, `Edit ${React.createElement('code'), {key: 3}, 'src/App.js'} and save to reload.`),
+        React.createElement('a', {className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer', key: 4}, 'Learn React'),
+        React.createElement('div', {key: 5}, `${new Date().getFullYear()}`)
+      ]
+    ))
+  )
 }
 
 export default App;
