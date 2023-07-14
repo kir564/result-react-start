c9ff6b5ac248680768f8f9e1d8008a69f151315a
kir564 <kir564ke@gmail.com>
2023-07-14 19:08:52 +0700

    calculator

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,21 +1,90 @@
-import classes from './calculator.module.css'
+import React, { useState } from 'react';
+import classes from './calculator.module.css';
+
+let operation = '';
 
 export function Calculator() {
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const operations = [ '+', '-', '=', 'C']
+  const [value, setValue] = useState('');
+  const [color, setColor] = useState('');
 
-    const buttons = [...numbers, ...operations].map(item => {
-        return <button className={classes.button} key={item}>{item}</button>
-    })
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const operations = ['+', '-', '=', 'C'];
 
+  const buttons = [...numbers, ...operations].map((item) => {
     return (
-        <div className={classes.calc}>
-            <div className={classes.display}>
+      <button className={classes.button} key={item}>
+        {item}
+      </button>
+    );
+  });
 
-            </div>
-            <div className={classes.buttons}>
-                {buttons}
-            </div>
-        </div>
-    )
+  function showValue(event) {
+    const isButton = event.target.closest(`.${classes.button}`);
+    const buttonValue = event.target.textContent;
+
+    if (isButton) {
+      if (buttonValue == 0) {
+        if (!value) return;
+      }
+
+      if (buttonValue === 'C') {
+        operation = '';
+        setValue('');
+      } else if (buttonValue === '=') {
+        if (!value) return;
+        if (!operation) return;
+
+        const operands = value.split(operation);
+        const result = calcResult(operands);
+        setColor('green');
+        setValue(result);
+      } else {
+        if (buttonValue === '+' || buttonValue === '-') {
+          if (!value) return;
+
+          if (operation) {
+            const operands = value.split(operation);
+            const result = calcResult(operands);
+            setColor('green');
+            setValue(result + buttonValue);
+            operation = buttonValue;
+
+            return;
+          }
+
+          operation = buttonValue;
+        }
+        setColor('');
+        setValue(value + buttonValue);
+      }
+    }
+  }
+
+  function calcResult(arr) {
+    let result;
+
+    switch (operation) {
+      case '+':
+        result = Number(arr[0]) + Number(arr[1]);
+        break;
+      case '-':
+        result = arr[0] - arr[1];
+        break;
+    }
+
+    operation = '';
+
+    return result;
+  }
+
+  return (
+    <div className={classes.calc}>
+      <div className={classes.display} style={{ color: color }}>
+        {value}
+      </div>
+      <div onClick={showValue} className={classes.buttons}>
+        {buttons}
+      </div>
+    </div>
+  );
 }
