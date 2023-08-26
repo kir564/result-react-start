1897485cbe2f3bf884908b108508de7b0a3481f4
kir564 <kir564ke@gmail.com>
2023-08-26 12:28:48 +0700

    request-change-tasks

--- src/request-change-tasks/request-change-tasks.js
+++ src/request-change-tasks/request-change-tasks.js
@@ -1,43 +1,37 @@
 import { ACTION, BASE_URL, PATH } from '../constants';
 
+const getParams = (method, title) => ({
+  method: method,
+  headers: { 'Content-Type': 'application/json;charset=utf-8' },
+  body: JSON.stringify({
+    title: title,
+  }),
+});
+
 export const requestChangeTasks = ({
   action,
   updateFlag,
-  id,
+  id = '',
   title,
   setIsLoadAction,
   navigate,
 }) => {
-  let url = '';
+  const url = `${BASE_URL.TASKS}/${id}`;
   let params = {};
+  
   setIsLoadAction(true);
 
   const { isUpdatedFlag, setIsUpdatedFlag } = updateFlag;
 
   if (action === ACTION.ADD) {
-    url = BASE_URL.TASKS;
-    params = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        title: title,
-      }),
-    };
+    params = getParams('POST', title);
   }
 
   if (action === ACTION.CHANGE) {
-    url = `${BASE_URL.TASKS}/${id}`;
-    params = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        title: title,
-      }),
-    };
+    params = getParams('PUT', title);
   }
 
   if (action === ACTION.DELETE) {
-    url = `${BASE_URL.TASKS}/${id}`;
     params = {
       method: 'DELETE',
     };
