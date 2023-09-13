b7466ec155a49ac40938b856a9fc3ea3b93cfafd
kir564 <kir564ke@gmail.com>
2023-09-13 13:52:20 +0700

    redux-devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
-import { legacy_createStore } from 'redux';
+import { legacy_createStore, compose, createStore } from 'redux';
 import { reducer } from './reducer';
 
-export const store = legacy_createStore(reducer);
+export const store = legacy_createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
